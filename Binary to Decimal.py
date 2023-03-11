78ee3391dc5054fe9d213b0bbc2514cc59cd4f3a
Astoriaaaa <114366090+Astoriaaaa@users.noreply.github.com>
2023-03-11 18:28:50 -0500

    Update Binary to Decimal.py

--- Binary to Decimal.py
+++ Binary to Decimal.py
@@ -1,27 +1,29 @@
 #bianary to decimal
-bianary_digit = (input( ))
-bd = bianary_digit[::-1]
-sum = 0 
-for i in range(len(bd)):  
-    if bd[i] == "0": 
-        pass
-    elif bd[i] == "1": 
-        sum = sum + (2 ** i)
-print(sum) 
+decision = int(input("Type '1' if you want to convert decimal to binary. Type '2' if you want to convert binary to decimal ")) 
+if decision == 2:
+    bianary_digit = (input("Input bianary number "))
+    bd = bianary_digit[::-1]
+    sum = 0 
+    for i in range(len(bd)):  
+        if bd[i] == "0": 
+            pass
+        elif bd[i] == "1": 
+            sum = sum + (2 ** i)
+    print("Your decimal digit is", sum) 
 
-#decimal to bianary
-bianary = " "
-number = int(input( ))
-num = number
-while num > 0.5: 
-    num = number / 2
-    if num % 1 == 0: 
-        bianary += "0"
-        number = num 
-    elif num % 0.5 == 0: 
-        bianary += "1"
-        number = number // 2
-print(bianary[::-1])
+elif decision == 1:
+    bianary = " "
+    number = int(input("input decimal number " ))
+    num = number
+    while num > 0.5: 
+        num = number / 2
+        if num % 1 == 0: 
+            bianary += "0"
+            number = num 
+        elif num % 0.5 == 0: 
+            bianary += "1"
+            number = number // 2
+    print("Your bianary digit it", bianary[::-1])
 
 
 
